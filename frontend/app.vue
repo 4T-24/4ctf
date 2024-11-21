347520a197e0f5bbbbeffb2406bfa1a0706b18ee
nathan <cornelie.nathanpro@gmail.com>
2024-11-21 17:37:07 +0100

    chore(init): init front nuxt vue vite nuxt-ui

---
+++ frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup lang="ts">
+import config from '@/data/config.yaml'
+
+console.log(config.project)
+</script>
